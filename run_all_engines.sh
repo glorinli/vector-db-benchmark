934108133f49199aa7eb643fd7515c5523cdd618
glorinli <ligr1991@gmail.com>
2024-10-22 17:12:10 +0800

    Run on 41.

--- run_all_engines.sh
+++ run_all_engines.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-DATASETS=${DATASETS:-"*"}
+DATASETS=${DATASETS:-"dbpedia-openai-100K-1536-angular"}
 
-SERVER_HOST=${SERVER_HOST:-"localhost"}
+SERVER_HOST=${SERVER_HOST:-"10.74.148.41"}
 
-SERVER_USERNAME=${SERVER_USERNAME:-"qdrant"}
+SERVER_USERNAME=${SERVER_USERNAME:-"root"}
 
 function run_exp() {
     SERVER_PATH=$1
     ENGINE_NAME=$2
     MONITOR_PATH=$(echo "$ENGINE_NAME" | sed -e 's/[^A-Za-z0-9._-]/_/g')
-    ssh "${SERVER_USERNAME}@${SERVER_HOST}" "nohup bash -c 'cd ./projects/vector-db-benchmark/monitoring && rm -f docker.stats.jsonl && bash monitor_docker.sh' > /dev/null 2>&1 &"
-    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d"
+    ssh "${SERVER_USERNAME}@${SERVER_HOST}" "nohup bash -c 'cd /projects/glorin.li/code/vector-db-benchmark/monitoring && rm -f docker.stats.jsonl && bash monitor_docker.sh' > /dev/null 2>&1 &"
+    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd /projects/glorin.li/code/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d"
     sleep 30
     python3 run.py --engines "$ENGINE_NAME" --datasets "${DATASETS}" --host "$SERVER_HOST"
-    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down"
-    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd ./projects/vector-db-benchmark/monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl"
+    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd /projects/glorin.li/code/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down"
+    ssh -t "${SERVER_USERNAME}@${SERVER_HOST}" "cd /projects/glorin.li/code/vector-db-benchmark/monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl"
 }
 
-
+run_exp "qdrant-single-node" 'qdrant-m-16-ef-128'
 #run_exp "qdrant-single-node" 'qdrant-m-*'
-run_exp "weaviate-single-node" 'weaviate-m-*'
-run_exp "milvus-single-node" 'milvus-m-*'
-run_exp "qdrant-single-node" 'qdrant-rps-m-*'
+#run_exp "weaviate-single-node" 'weaviate-m-*'
+#run_exp "milvus-single-node" 'milvus-m-*'
+#run_exp "qdrant-single-node" 'qdrant-rps-m-*'
 
 
 # run_exp "elasticsearch-single-node" 'elastic-m-*'
