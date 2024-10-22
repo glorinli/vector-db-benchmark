6f756057dce01d6d68a8b5d352b34136942f847d
glorinli <ligr1991@gmail.com>
2024-10-22 16:44:25 +0800

    Support local run.

--- run_all_engines_local.sh
+++ run_all_engines_local.sh
@@ -10,12 +10,18 @@ function run_exp() {
     SERVER_PATH=$1
     ENGINE_NAME=$2
     MONITOR_PATH=$(echo "$ENGINE_NAME" | sed -e 's/[^A-Za-z0-9._-]/_/g')
+    echo "Starting server ${SERVER_PATH} ..."
     nohup bash -c 'cd ./monitoring && rm -f docker.stats.jsonl && bash monitor_docker.sh' > /dev/null 2>&1 &
-    bash -c 'cd ./engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d'
+    bash -c "cd ./engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d"
     sleep 30
+    echo 'Activate poetry'
+    source $(poetry env info --path)/bin/activate
+    which python
+    echo 'Run experiments...'
     python3 run.py --engines "$ENGINE_NAME" --datasets "${DATASETS}" --host "$SERVER_HOST"
-    bash -c 'cd ./engine/servers/$SERVER_PATH ; docker compose down'
-    bash -c 'cd ./monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl'
+    echo 'Shutdown server...'
+    bash -c "cd ./engine/servers/$SERVER_PATH ; docker compose down"
+    bash -c "cd ./monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl"
 }
 
 
