bf65985ef2273818574a90b66b3cbd60c578ed3d
glorinli <ligr1991@gmail.com>
2024-11-04 10:52:48 +0800

    Update configs.

--- dataset_reader/ann_compound_reader.py
+++ dataset_reader/ann_compound_reader.py
@@ -83,7 +83,16 @@ class AnnCompoundReader(JSONReader):
         else:
             mock_meta_conditions = [None]
 
+        for condition in mock_meta_conditions:
+            if condition:
+                and_cond = condition["and"]
+                and_cond.append({"type": {"match": {"value": "kbarticle"}}})
+                and_cond.append({"chunking_strategy": {"match": {"value": "default"}}})
+
         print(f"Condition count: {len(mock_meta_conditions)}")
+
+        if mock_meta_conditions[0]:
+            print(f"Condition: {mock_meta_conditions[0]}")
 
         for condition in mock_meta_conditions:
             with open(self.path / self.QUERIES_FILE) as payloads_fp:
